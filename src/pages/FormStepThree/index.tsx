bf8e891b1c288697aa4426de0a140d26faf2d0e0
Patrick <patrickpqdt87289@gmail.com>
2022-03-08 20:37:44 -0300

    Completed Pages

--- src/pages/FormStepThree/index.tsx
+++ src/pages/FormStepThree/index.tsx
@@ -1,11 +1,80 @@
-import { Theme } from '../../themes';
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
 
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../themes';
+import { ChangeEvent, useEffect } from 'react';
+
 export const FormStep3 = () => {
+
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+
+        if (state.name === '') {
+            navigate('/');
+        }
+        else {
+            dispatch({
+                type: FormActions.setCurrentStep,
+                payload: 3
+            })
+        }
+        
+    },[dispatch])
+
+    const handleNextStep = () => {
+       if (state.email && state.github !== '') {
+           alert(`Ok ${state.name} agora é com a empresa!`)
+           console.log(state)
+       }
+       else {
+           navigate('/');
+       }
+    }
+
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setEmail,
+            payload: e.target.value 
+        });
+    }
+    const handleGithubChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setGithub,
+            payload: e.target.value 
+        });
+    }
+
     return (
-       <Theme>
+        <Theme>
             <C.Container>
-                Passo 3
+                <p>Passo {state.currentStep}/3</p>
+                <h1>Legal {state.name}, onde te achamos ?</h1>
+                <p>Preencha os campos para entrarmos em contato quando houver um Job.</p>
+
+                <hr/>
+
+                <label>
+                    Qual é o seu E-mail ?
+                    <input
+                        type="email"
+                        value={state.email}
+                        onChange={handleEmailChange}
+                    />
+                </label>
+
+                <label>
+                    Qual é o seu Github ?
+                    <input
+                        type="url"
+                        value={state.github}
+                        onChange={handleGithubChange}
+                    />
+                </label>
+                 <C.BackButton to="/step2">Voltar</C.BackButton>
+                <button onClick={handleNextStep}>Finalizar Cadastro</button>
             </C.Container>
         </Theme>
     );
--- src/pages/FormStepTwo/index.tsx
+++ src/pages/FormStepTwo/index.tsx
@@ -1,11 +1,76 @@
-import { Theme } from '../../themes';
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
 
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../themes';
+import {  useEffect } from 'react';
+
+import { SelectOption } from '../../components/SelectOption';
+
 export const FormStep2 = () => {
+
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+
+        if (state.name === '') {
+            navigate('/')
+        }
+        else {
+            dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 2
+        })
+        }
+
+        
+    },[dispatch])
+
+    const handleNextStep = () => {
+        if (state.name !== '') {
+            navigate('/step3');
+        }
+        else {
+            alert('Preencha os dados.')
+        }
+    }
+    
+    const setLevel = (level: number) => {
+        dispatch({
+            type: FormActions.setLevel,
+            payload: level
+        })
+    }
+
+
     return (
         <Theme>
             <C.Container>
-                Passo 2
+                <p>Passo {state.currentStep}/3</p>
+                <h1>{state.name}, o que melhor descreve você ?</h1>
+                <p>Escolha a opção que define o seu estato atual, profissionalmente.</p>
+
+                <hr/>
+
+                <SelectOption
+                    title="Sou iniciante"
+                    description="Comecei a programar há menos de 2 anos"
+                    icon="👦"
+                    selected={state.level === 0}
+                    onClick={() => setLevel(0)}
+                />
+
+                <SelectOption
+                    title="Sou Programador"
+                    description="Já Programo há 2 anos ou mais."
+                    icon="👨‍🚀"
+                    selected={state.level === 1}
+                    onClick={() => setLevel(1)}
+                />
+
+                <C.BackButton to="/">Voltar</C.BackButton>
+                <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
     );
